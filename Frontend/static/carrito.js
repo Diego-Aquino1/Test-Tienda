f0c7bd4c3e914bcc3b5f09b043b44a7333a8a908
Diego-Aquino1 <daquino@unsa.edu.pe>
2023-06-16 18:52:31 -0500

    carritov2

--- Frontend/static/carrito.js
+++ Frontend/static/carrito.js
@@ -4,11 +4,15 @@ const envioInput = document.getElementById("envio");
 const recojoInput = document.getElementById("recojo");
 const formEnvio = document.getElementById("envio_main");
 const formRecojo = document.getElementById("recojo_main");
+const cenvio = document.getElementsByClassName("envio1");
+const crecojo = document.getElementsByClassName("recojo1");
 
 envioInput.addEventListener("change", function() {
   if (envioInput.checked) {
     formEnvio.style.display = "block";
     formRecojo.style.display = "none";
+    cenvio[0].classList.add("activo");
+    crecojo[0].classList.remove("activo");
   }
 });
 
@@ -16,6 +20,8 @@ recojoInput.addEventListener("change", function() {
   if (recojoInput.checked) {
     formEnvio.style.display = "none";
     formRecojo.style.display = "block";
+    cenvio[0].classList.remove("activo");
+    crecojo[0].classList.add("activo");
   }
 });
 
