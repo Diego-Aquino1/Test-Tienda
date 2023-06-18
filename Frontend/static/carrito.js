92b5e4d83a89b1b473ef5eeccb824740ed05626b
Diego-Aquino1 <daquino@unsa.edu.pe>
2023-06-17 20:16:39 -0500

    carrito v3

--- Frontend/static/carrito.js
+++ Frontend/static/carrito.js
@@ -7,22 +7,34 @@ const formRecojo = document.getElementById("recojo_main");
 const cenvio = document.getElementsByClassName("envio1");
 const crecojo = document.getElementsByClassName("recojo1");
 
-envioInput.addEventListener("change", function() {
-  if (envioInput.checked) {
-    formEnvio.style.display = "block";
-    formRecojo.style.display = "none";
-    cenvio[0].classList.add("activo");
-    crecojo[0].classList.remove("activo");
-  }
-});
+recojoInput.checked = true;
+formEnvio.style.display = "none";
+formRecojo.style.display = "block";
+cenvio[0].classList.remove("activo");
+crecojo[0].classList.add("activo");
 
-recojoInput.addEventListener("change", function() {
-  if (recojoInput.checked) {
-    formEnvio.style.display = "none";
-    formRecojo.style.display = "block";
-    cenvio[0].classList.remove("activo");
-    crecojo[0].classList.add("activo");
-  }
-});
+function activarEnvio() {
+  formEnvio.style.display = "block";
+  formRecojo.style.display = "none";
+  cenvio[0].classList.add("activo");
+  crecojo[0].classList.remove("activo");
+  envioInput.checked = true;
+  recojoInput.checked = false;
+}
+
+function activarRecojo() {
+  formEnvio.style.display = "none";
+  formRecojo.style.display = "block";
+  cenvio[0].classList.remove("activo");
+  crecojo[0].classList.add("activo");
+  envioInput.checked = false;
+  recojoInput.checked = true;
+}
+
+cenvio[0].addEventListener("click", activarEnvio);
+crecojo[0].addEventListener("click", activarRecojo);
+
+envioInput.addEventListener("change", activarEnvio);
+recojoInput.addEventListener("change", activarRecojo);
 
 });
